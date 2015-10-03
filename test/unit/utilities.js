b82cc748f275c9b052dd36550879f386fe73e9dd
Livvie Lin <livvie.lin@gmail.com>
2015-10-04 07:59:17 +0800

    Write unit tests for utils

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -1,6 +1,7 @@
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
+// var util = require('../../client/scripts/todo/todo.util');
 
 describe('the todo.App', function() {
   describe('the todo object', function(){
@@ -11,4 +12,37 @@ describe('the todo.App', function() {
 });
 
 describe('the todo.util methods', function() {
+  describe('trimTodoName', function(){
+    it('should return a string', function(){
+      var trimmed = todo.util.trimTodoName('Eat pie');
+      assert.typeOf(trimmed, 'string');
+    });
+
+    it('should trim a todo name', function(){
+      var trimmed = todo.util.trimTodoName('Eat pie ');
+      trimmed.should.equal('Eat pie');
+      trimmed.should.have.length(7);
+    });
+  });
+
+  describe('isValidTodoName', function(){
+    it('should return true when valid name given', function(){
+      var isValid = todo.util.isValidTodoName('Eat pie');
+      expect(isValid).to.equal(true);
+    });
+
+    it('should return false when invalid name given', function(){
+      var isValid = todo.util.isValidTodoName('A ');
+      expect(isValid).to.not.equal(true);
+    });
+  });
+
+  describe('getUniqueId', function(){
+    it('should', function(){
+      var firstId = todo.util.getUniqueId();
+      firstId.should.equal(1);
+      var secondId = todo.util.getUniqueId();
+      secondId.should.equal(2);
+    });
+  });
 });
