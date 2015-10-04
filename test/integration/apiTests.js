b3dde6bd23aa805877b66232c53b55712ed7acee
Livvie Lin <livvie.lin@gmail.com>
2015-10-04 08:35:25 +0800

    Setup stub with fake data

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,12 +1,32 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  var fakeData = [
+    {
+      id: 1,
+      name: 'Eat pie',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Play soccer',
+      done: false
+    },
+    {
+      id: 3,
+      name: 'Code',
+      done: false
+    }
+  ];
+
   before(function () {
     server = sinon.fakeServer.create();
+    setupStub = sinon.stub($, 'ajax').yieldTo('success', fakeData);
   });
 
   after(function () {
     server.restore();
+    $.ajax.restore();
   });
 
   it('todo.setup receives an array of todos when todo.init is called', function () {
