d86103789c6dfbc41930c73e6f6695c282390ae6
Livvie Lin <livvie.lin@gmail.com>
2015-10-04 08:17:14 +0800

    Create fake server using sinon

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,14 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  before(function () {
+    server = sinon.fakeServer.create();
+  });
+
+  after(function () {
+    server.restore();
+  });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
   });
 });
