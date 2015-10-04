48242df7d1d0a793c407921bbf9fb2165bd235cb
Livvie Lin <livvie.lin@gmail.com>
2015-10-04 09:39:41 +0800

    Set up casper tests

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -10,4 +10,46 @@ casper.test.begin('App is setup correctly', 2, function suite(test) {
   });
 });
 
+// Test that adds and removes todo items
+casper.test.begin('Adds and removes todo items', 3, function suite(test) {
+  casper.start('http://localhost:3000/', function() {
+    //
+  });
 
+  casper.run(function() {
+    test.done();
+  });
+});
+
+// Test that adds and removes multiple todo items
+casper.test.begin('Adds and removes multiple todo items', 3, function suite(test) {
+  casper.start('http://localhost:3000/', function() {
+    //
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+// Test that marks todo items as done
+casper.test.begin('Marks todo items as done', 3, function suite(test) {
+  casper.start('http://localhost:3000/', function() {
+    //
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+// Test that ensures user cannot add empty todo items
+casper.test.begin('Ensures user cannot add empty todo items', 3, function suite(test) {
+  casper.start('http://localhost:3000/', function() {
+    //
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
